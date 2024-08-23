f6801cb18c2b6506fdd08ddfd9582c8737cf4854
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-08-23 01:40:53 +0000

    HACKING-MD

--- pair.js
+++ pair.js
@@ -11,7 +11,7 @@ const {
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require("gifted-baileys");
+} = require("@sampandey001/baileys");
 
 function removeFile(FilePath){
     if(!fs.existsSync(FilePath)) return false;
