1b723db1c2eece47f79e112031869c438124e2e4
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-08-24 19:52:34 +0000

    HACKING-MD

--- qr.js
+++ qr.js
@@ -14,7 +14,7 @@ const {
 	Browsers,
 	delay,
 	makeInMemoryStore,
-} = require("gifted-baileys");
+} = require("@whiskeysockets/baileys");
 
 function removeFile(FilePath) {
 	if (!fs.existsSync(FilePath)) return false;
